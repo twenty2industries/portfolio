f6b8405b77c76247c03649906de3cca09290565e
twenty2industries <burak.bostanli.1@web.de>
2025-06-18 08:51:35 +0200

    Style: header added & hero section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent],
+  imports: [RouterOutlet, MainContentComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
