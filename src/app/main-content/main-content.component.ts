cdfa8c5a00243fbf9e9583a140225f5de12bd630
twenty2industries <burak.bostanli.1@web.de>
2025-06-18 19:17:57 +0200

    style: myProjects component almost done

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 
 
 @Component({
   selector: 'app-main-content',
-  imports: [WhyMeComponent, MySkillsComponent],
+  imports: [WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
