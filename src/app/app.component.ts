42ad2acdcc69f912eb1b2e1ef9d6e4ff5d05d091
twenty2industries <burak.bostanli.1@web.de>
2025-06-20 13:48:44 +0200

    feat&style: shared/footer.component has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HeroComponent } from './main-content/hero/hero.component';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, MainContentComponent, HeaderComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
