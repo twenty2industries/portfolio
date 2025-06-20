453688f1534887acdcfab2893bce759ad43ae550
twenty2industries <burak.bostanli.1@web.de>
2025-06-20 08:23:21 +0200

    feat: shared input box added

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputBoxComponent } from '../../shared/input-box/input-box.component';
+
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [InputBoxComponent],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
---
+++ src/app/shared/input-box/input-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-box',
+  imports: [],
+  templateUrl: './input-box.component.html',
+  styleUrl: './input-box.component.scss'
+})
+export class InputBoxComponent {
+
+}
