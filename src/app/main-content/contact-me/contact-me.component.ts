44d422e4c5169eff3d87028bdb89bd26a421c384
twenty2industries <burak.bostanli.1@web.de>
2025-06-20 14:14:48 +0200

    feat: /main-content/contact-me.component added

---
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-me',
+  imports: [],
+  templateUrl: './contact-me.component.html',
+  styleUrl: './contact-me.component.scss'
+})
+export class ContactMeComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,13 +3,15 @@ import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
 
 
 
 
 @Component({
   selector: 'app-main-content',
-  imports: [WhyMeComponent, MySkillsComponent, MyProjectsComponent, FeedbackComponent],
+  imports: [WhyMeComponent, MySkillsComponent, MyProjectsComponent, FeedbackComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
