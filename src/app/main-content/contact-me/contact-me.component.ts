0c2b9d8927d9726df85f075c00e94fc6c094651c
twenty2industries <burak.bostanli.1@web.de>
2025-06-25 15:06:51 +0200

    feat: contact-me.components edited to <form> attribute. method onSubmit(contactForm) working properly. to be added: Display input requirements & errors if requirments are not fulfilled

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,19 +1,26 @@
 import { Component, Input } from '@angular/core';
-import { ContactFormData } from "./../interfaces/contactMe.interface";
+import { ContactFormData } from './../interfaces/contactMe.interface';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
-@Input() contactData!: ContactFormData;
+  @Input() contactData!: ContactFormData;
 
+  contactDatas = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
-  isInputActive = false;
-
-  toggleInputFocus() {
-    this.isInputActive = !this.isInputActive;
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+          console.log(this.contactDatas);
+    }
   }
 }
